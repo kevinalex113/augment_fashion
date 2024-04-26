02bea987a6a0df2579e47a0275e3f3629656e7b8
EduardoBautista9916 <eduardobautista9916@gmail.com>
2024-04-26 16:28:49 -0600

    change_CartModel

--- src/main/java/com/shop/augment_fashion/models/CartModel.java
+++ src/main/java/com/shop/augment_fashion/models/CartModel.java
@@ -1,13 +1,24 @@
 package com.shop.augment_fashion.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name="TBL_CART")
 public class CartModel {
+
+    @Setter @Getter @Column(name="NID_USER")
     private int nid_user;
+
+    @Setter @Getter @Column(name="NID_CLOTHES")
     private int nid_clothes;
+
+    @Setter @Getter @Column(name="NQUANTITY")
     private int nquantity;
+
+    @Setter @Getter @Column(name="FAMOUNT")
     private float famount;
 }
